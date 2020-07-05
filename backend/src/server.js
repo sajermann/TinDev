e1bbaf75dfba4cdb684d1dd56a50c96035526e81
Bruno Sajermann <contato@sajermann.com>
2020-07-05 13:42:24 -0300

    Day 2 Complete

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+const routes = require('./routes')
+const server = express();
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k6x9m.gcp.mongodb.net/oministack8?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+})
+server.use(cors());
+server.use(express.json())
+server.use(routes);
+
+server.listen(3333);
