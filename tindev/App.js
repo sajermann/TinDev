c01f7f4e9ab3b3886f7be0533f5e975f8f3f2028
Bruno Sajermann <contato@sajermann.com>
2020-07-07 22:57:23 -0300

    Day 4 Complete

---
+++ tindev/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Index from './src/index';
+
+export default function App() {
+  return (
+    <Index />
+    
+  );
+}
